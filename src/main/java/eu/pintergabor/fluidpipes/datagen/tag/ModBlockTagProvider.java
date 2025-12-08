886ed8b9bcc18349abd1a8281af1ab4fd102049c
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-08 12:13:06 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/fluidpipes/datagen/tag/ModBlockTagProvider.java
+++ src/main/java/eu/pintergabor/fluidpipes/datagen/tag/ModBlockTagProvider.java
@@ -29,25 +29,25 @@ public final class ModBlockTagProvider extends FabricTagProvider.BlockTagProvide
 	protected void addTags(@NotNull HolderLookup.Provider wrapperLookup) {
 		// Remove pipes and fittings only with a pickaxe,
 		// and wooden pipes with an axe too.
-		getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_AXE)
+		valueLookupBuilder(BlockTags.MINEABLE_WITH_AXE)
 			.addOptionalTag(ModBlockTags.WOODEN_PIPES)
 			.addOptionalTag(ModBlockTags.WOODEN_FITTINGS);
-		getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
+		valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
 			.addOptionalTag(ModBlockTags.STONE_PIPES)
 			.addOptionalTag(ModBlockTags.STONE_FITTINGS)
 			.addOptionalTag(ModBlockTags.WOODEN_PIPES)
 			.addOptionalTag(ModBlockTags.WOODEN_FITTINGS);
 		// Wooden pipes.
-		getOrCreateTagBuilder(ModBlockTags.WOODEN_PIPES)
+		valueLookupBuilder(ModBlockTags.WOODEN_PIPES)
 			.add(ModFluidBlocks.WOODEN_PIPES);
 		// Wooden fittings.
-		getOrCreateTagBuilder(ModBlockTags.WOODEN_FITTINGS)
+		valueLookupBuilder(ModBlockTags.WOODEN_FITTINGS)
 			.add(ModFluidBlocks.WOODEN_FITTINGS);
 		// Stone pipes.
-		getOrCreateTagBuilder(ModBlockTags.STONE_PIPES)
+		valueLookupBuilder(ModBlockTags.STONE_PIPES)
 			.add(ModFluidBlocks.STONE_PIPES);
 		// Stone fittings.
-		getOrCreateTagBuilder(ModBlockTags.STONE_FITTINGS)
+		valueLookupBuilder(ModBlockTags.STONE_FITTINGS)
 			.add(ModFluidBlocks.STONE_FITTINGS);
 	}
 }
