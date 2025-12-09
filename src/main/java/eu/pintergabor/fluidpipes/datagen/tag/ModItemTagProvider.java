45456200ec5f6f0c085bd54f7b3aa5b9ca35f6a0
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-09 17:05:04 +0100

    Refactoring.

--- src/main/java/eu/pintergabor/fluidpipes/datagen/tag/ModItemTagProvider.java
+++ src/main/java/eu/pintergabor/fluidpipes/datagen/tag/ModItemTagProvider.java
@@ -1,10 +1,16 @@
 package eu.pintergabor.fluidpipes.datagen.tag;
 
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+
 import eu.pintergabor.fluidpipes.Global;
 import eu.pintergabor.fluidpipes.block.BaseBlock;
 import eu.pintergabor.fluidpipes.registry.ModFluidBlocks;
 import eu.pintergabor.fluidpipes.registry.variants.ModBlockVariant;
 import eu.pintergabor.fluidpipes.tag.ModItemTags;
+import net.neoforged.neoforge.common.data.ItemTagsProvider;
+import org.jetbrains.annotations.NotNull;
+
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagAppender;
@@ -12,20 +18,15 @@ import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.common.data.ItemTagsProvider;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Arrays;
-import java.util.concurrent.CompletableFuture;
 
 
 public final class ModItemTagProvider extends ItemTagsProvider {
 
 	@SuppressWarnings("unused")
 	public ModItemTagProvider(
-			PackOutput output,
-			CompletableFuture<HolderLookup.Provider> lookupProvider,
-			CompletableFuture<TagsProvider.TagLookup<Block>> blockTagProvider
+		PackOutput output,
+		CompletableFuture<HolderLookup.Provider> lookupProvider,
+		CompletableFuture<TagsProvider.TagLookup<Block>> blockTagProvider
 	) {
 		super(output, lookupProvider, Global.MODID);
 	}
@@ -34,13 +35,11 @@ public final class ModItemTagProvider extends ItemTagsProvider {
 	 * Add an array of blocks as items to an item tag.
 	 */
 	private void add(
-			TagKey<Item> key,
-			ModBlockVariant<BaseBlock>[] blocks
+		TagKey<Item> key,
+		ModBlockVariant<BaseBlock>[] blocks
 	) {
 		final TagAppender<Item, Item> builder = tag(key);
-		//Arrays.stream(blocks).forEach(b -> builder.add(Item.byBlock(b.get())));
 		builder.addAll(Arrays.stream(blocks).map(ModBlockVariant::getItem));
-		//Arrays.stream(blocks).map(deferredBlock -> deferredBlock.getItem()).forEach(builder::add);
 	}
 
 	/**
@@ -56,9 +55,9 @@ public final class ModItemTagProvider extends ItemTagsProvider {
 		add(ModItemTags.STONE_FITTINGS, ModFluidBlocks.STONE_FITTINGS);
 		// All pipes and fittings.
 		tag(ModItemTags.PIPES_AND_FITTINGS)
-				.addTag(ModItemTags.WOODEN_PIPES)
-				.addTag(ModItemTags.WOODEN_FITTINGS)
-				.addTag(ModItemTags.STONE_PIPES)
-				.addTag(ModItemTags.STONE_FITTINGS);
+			.addTag(ModItemTags.WOODEN_PIPES)
+			.addTag(ModItemTags.WOODEN_FITTINGS)
+			.addTag(ModItemTags.STONE_PIPES)
+			.addTag(ModItemTags.STONE_FITTINGS);
 	}
 }
